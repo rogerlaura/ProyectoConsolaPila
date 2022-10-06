46c3f57e9d333fa0ee38b50d30ad71748e9982f5
Roger Laura <roylauracaceres@gmail.com>
2022-10-06 08:19:05 -0400

    Agregar archivos de proyecto.

---
+++ ProyectoConsolaPila/Pila.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoConsolaPila
+{
+    internal class Pila
+    {
+        private int tope,maxpila;
+        private int[] contenedorpila;
+        public Pila(int maxpila)
+        {
+            this.tope = -1;
+            this.maxpila = maxpila;
+            this.contenedorpila = new int[maxpila];
+        }
+        public bool Pilavacia()
+        {
+            if (this.tope == -1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool Pilallena()
+        {
+            if (this.tope == maxpila - 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void ApilarPila(int dato)
+        {
+            if (this.Pilallena() == false)
+            {
+                this.tope++;
+                this.contenedorpila[tope] = dato;
+                Console.WriteLine("\tSE APILO EL ELEMENTO "+this.contenedorpila[tope] + "EN LA PILA");
+            }
+            else
+            {
+                Console.WriteLine("\tLA PILA ESTA LLENA");
+            }
+        }
+        public void DesapilarPila()
+        {
+            if (this.Pilavacia() == false)
+            {
+                Console.WriteLine("\tSE DASAPILO EL ELEMENTO " + this.contenedorpila[tope]+" DE LA PILA ");
+                this.contenedorpila[tope] = 0;
+                this.tope--;
+            }
+            else
+            {
+                Console.WriteLine("\tLA PILA ESTA VACIA");
+            }
+        }
+        public void ListarPila()
+        {
+            if(this.Pilavacia() == false)
+            {
+                for (int i = tope; i >=0; i--)
+                {
+                    Console.WriteLine("[" + i + "] => " + this.contenedorpila[i]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("\tLA PILA ESTA VACIA");
+            }
+        }
+
+
+    }
+}
---
+++ ProyectoConsolaPila/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoConsolaPila
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dato, opcion;
+            Pila pila = new Pila(5);
+            do
+            {
+                Console.WriteLine("\t\t============MENU============");
+                Console.WriteLine("\t\t1.- APILAR");
+                Console.WriteLine("\t\t2.- DASAPILAR");
+                Console.WriteLine("\t\t3.- LISTAR PILA");
+                Console.WriteLine("\t\t4.- SALIR");
+                Console.Write("\tELIJA UNA OPCION: ");
+                opcion = int.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        Console.Write("\t\tDIGITE EL VALOR DEL ELEMENTO: ");
+                        dato = int.Parse(Console.ReadLine());
+                        pila.ApilarPila(dato);
+                        break;
+                    case 2:
+                        pila.DesapilarPila();
+                        break;
+                    case 3:
+                        pila.ListarPila();
+                        break;
+
+                }
+                Console.ReadKey(true);
+                Console.Clear();
+                
+            } while (opcion!=4);
+
+
+
+            Console.ReadKey(true);
+        }
+    }
+}
